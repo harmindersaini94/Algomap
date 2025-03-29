8794d88321bd78c4f4d03f10baf939525b87886e
Harminder Saini <63682981+harmindersaini94@users.noreply.github.com>
2025-03-29 15:27:11 +0000

    09_Merge_Intervals

--- Arrays & Strings/09_Merge_Intervals.py
+++ Arrays & Strings/09_Merge_Intervals.py
@@ -3,30 +3,22 @@ def merge(self, intervals):
         :type intervals: List[List[int]]
         :rtype: List[List[int]]
         """
-        final_list = []
-        n = len(intervals)
-        merged = []
 
-        if n == 1:
-            return intervals
+        merged_list = []
 
-        for item in range(1, n):
-            list_i_1 = intervals[item-1] #[8,10]
-            list_i = intervals[item] #[15,18]
+        #sort intervals first based on the starting point
+        intervals.sort(key=lambda x:x[0])
 
-            if list_i_1[1] >= list_i[0]:
-                final_list.append([list_i_1[0], list_i[1]]) #[1,6] [8,10]
-                merged = list_i #[]
+        for x in intervals:
+            if len(merged_list) > 0:
+                y = merged_list[-1]
+
+                if y[1] >= x[0]:
+                    merged_list[-1] = [y[0], max(y[1], x[1])]
+                else: merged_list.append(x)
             else:
-                if list_i_1 == merged:
-                    #final_list.append(list_i) #[1,6] [8,10]
-                    merged = []
-                else: final_list.append(list_i_1)
+                # means merged list is empty, it will be during the start
+                merged_list.append(x)
 
-                if list_i == merged:
-                    #final_list.append(list_i) #[1,6] [8,10]
-                    merged = []
-                else: final_list.append(list_i)
-
-        return final_list
-
+        return merged_list
+        
