92465c6957b8ac49bdecf92703e81de55417e19d
Harminder Saini <63682981+harmindersaini94@users.noreply.github.com>
2025-04-17 22:25:17 +0000

    05_MinStack_Updated

--- Stack & Queue/05_Min_Stack.py
+++ Stack & Queue/05_Min_Stack.py
@@ -1,14 +1,23 @@
 class MinStack:
     def __init__(self):
         self.stack = []
+        self.min_stack = []
         
 
     def push(self, val: int) -> None:
-        self.stack.append(val)
+        if len(self.stack) == 0:
+            self.stack.append(val)
+            self.min_stack.append(val)
+        else:
+            self.stack.append(val)
+            if self.min_stack[-1] < val:
+                self.min_stack.append(self.min_stack[-1])
+            else: self.min_stack.append(val)
         
 
     def pop(self) -> None:
         self.stack.pop()
+        self.min_stack.pop()
         
 
     def top(self) -> int:
@@ -16,7 +25,7 @@ class MinStack:
         
 
     def getMin(self) -> int:
-        return min(self.stack)
+        return self.min_stack[-1]
         
 
 
